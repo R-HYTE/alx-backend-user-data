80e9078f2fd832e1406512527441f84c7625ee7d
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-31 09:16:49 +0300

    Add RedactingFormatter class to handle log message redaction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ redaction string using regular expressions.
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -26,3 +27,46 @@ def filter_datum(
     """
     pattern = f"({'|'.join(fields)})=([^{separator}]+)"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    This class formats log messages and redacts sensitive information
+    specified in the fields list.
+
+    Attributes:
+        REDACTION (str): The string used to redact sensitive information.
+        FORMAT (str): The format of the log messages.
+        SEPARATOR (str): Separator used to split the fields in the log message.
+        fields (List[str]): List of fields to be redacted in the log messages.
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Initializes the RedactingFormatter with the specified fields to redact.
+
+        Args:
+            fields (List[str]): The list of field names to be redacted.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record and redacts specified sensitive fields.
+
+        Args:
+            record (logging.LogRecord): log record to be formatted and redacted
+
+        Returns:
+            str: The formatted and redacted log message.
+        """
+        original_message = super().format(record)
+        redacted_message = filter_datum(
+                self.fields, self.REDACTION, original_message, self.SEPARATOR
+        )
+        return redacted_message
