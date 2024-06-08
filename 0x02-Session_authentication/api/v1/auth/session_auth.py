669cbf2c1fd38c79492dc7d6d3224bfdf8cecc17
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-08 20:54:37 +0300

    feat: Add session-based user authentication in  class using session cookie

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -19,6 +20,7 @@ class SessionAuth(Auth):
 
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
+
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
@@ -28,3 +30,17 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Return a User instance based on a cookie value
+        """
+
+        cookie_value = self.session_cookie(request)
+        if cookie_value is None or not isinstance(cookie_value, str):
+            return None
+
+        user_id = self.user_id_for_session_id(cookie_value)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
